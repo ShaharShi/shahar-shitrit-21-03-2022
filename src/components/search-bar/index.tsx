620da6a6c411942925b8521b1f964efd3b93b008
שחר שטרית <shaharshitrit@h-MacBook-Pro-sl-shr.local>
2022-04-20 15:18:59 +0300

    fix search autocomplete option

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,7 +1,6 @@
-import { createRef, SyntheticEvent, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styles from './style.module.css'
-import { IoIosSearch } from 'react-icons/io'
 
 interface ISearchBar {
     termLocations: IPartialLocation[];
@@ -15,17 +14,18 @@ export default function SearchBar (props: ISearchBar) {
     const [citiesComplete, setCitiesComplete] = useState<IPartialLocation[]>([])
 
     const delaySearch = () => setTimeout(() => props.searchLocations(searchTerm), 500);
+    const setCity = (city: IPartialLocation) => props.setLocation(city);
 
     function setCities() {
         const citiesArr: IPartialLocation[] = props.termLocations;
         setCitiesComplete(citiesArr)
     }
-    function handleClick(e: any) {
-        const { setLocation } = props;
-    }
+
     useEffect(() => setCities(), [props.termLocations])
     useEffect(() => {
         if (!searchTerm.length) return setCitiesComplete([]);
+        const city = citiesComplete.find(c => c.locationName === searchTerm);
+        if (city) return setCity(city)
 
         const delayDebounceTimeout = delaySearch()
         return () => clearTimeout(delayDebounceTimeout)
@@ -36,14 +36,9 @@ export default function SearchBar (props: ISearchBar) {
                 <div className='flex-grow-1'>
                     <input type='text' className={`${styles.searchInput} ${isDarkMode ? 'text-light' : 'text-dark'}`} onChange={(e) => setSearchTerm(e.target.value)} list={'cities'} name={'term'} placeholder={'Search for a location ...'}/>
                     <datalist id={'cities'}>
-                        { citiesComplete.map(city => <option data-value={city.id} key={city.id}>{ city.locationName }</option>)}
+                        { citiesComplete.map(city => <option key={city.id}>{ city.locationName }</option>)}
                     </datalist>
                 </div>
-            </div>
-            <div className={`w-25 d-flex justify-content-center align-items-center mx-4 ${isDarkMode ? 'background-dark text-light box-shadow-dark' : 'background-light text-dark box-shadow-light'}`}>
-                <button className={styles.searchBtn} onClick={handleClick}>
-                    <IoIosSearch size={40}/>
-                </button>
             </div>
         </div>
     )
